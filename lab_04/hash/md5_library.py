b839a0dc716e225f2b45e53c9022359b2b7e5290
KostasArkride <zorosofgamer900@gmail.com>
2025-04-05 10:25:33 +0700

    Add files via upload

---
+++ lab_04/hash/md5_library.py
@@ -0,0 +1,25 @@
+<<<<<<< HEAD
+import hashlib
+
+def calculate_md5(input_string):
+    md5_hash = hashlib.md5()
+    md5_hash.update(input_string.encode('utf-8'))
+    return md5_hash.hexdigest()
+
+input_string = input("Nhập chuỗi cần băm: ")
+md5_hash = calculate_md5(input_string)
+
+print("Mã băm MD5 của chuỗi '{}' là: {}".format(input_string, md5_hash))
+=======
+import hashlib
+
+def calculate_md5(input_string):
+    md5_hash = hashlib.md5()
+    md5_hash.update(input_string.encode('utf-8'))
+    return md5_hash.hexdigest()
+
+input_string = input("Nhập chuỗi cần băm: ")
+md5_hash = calculate_md5(input_string)
+
+print("Mã băm MD5 của chuỗi '{}' là: {}".format(input_string, md5_hash))
+>>>>>>> 7643494659739fc8d5214eb15562c4496023e197
